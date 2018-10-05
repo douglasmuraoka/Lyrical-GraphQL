9fe790ba50cceaa31c01d0290247bc25fde9b777
Douglas Muraoka <douglas.muraoka@gmail.com>
2018-10-04 21:15:19 -0300

    feat: Add mutation to delete a song
    Adds a mutation into `SongList` component that can delete a song, given an ID. The component should automatically update itself with the new list of songs (once the mutation is complete), and remove the song from its view.

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,12 +3,30 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import fetchSongsQuery from '../queries/fetchSongs';
+import gql from 'graphql-tag';
 
 class SongList extends Component {
+  onSongDelete (id) {
+    this.props.mutate({
+      variables: {
+        id
+      }
+    }).then(() => {
+      // "refetch" is provided by Apollo for refetching
+      // all queries from THIS component. It is an alternative
+      // for "refetchQueries" when we need to refetch the
+      // initial queries of an updated component.
+      // In this case, we will execute the "fetchSongsQuery"
+      // and update our view with the updated data.
+      this.props.data.refetch()
+    });
+  }
+
   renderSongs () {
-    return this.props.data.songs.map(song => (
-      <li key={song.id} className="collection-item">
-        {song.title}
+    return this.props.data.songs.map(({ id, title }) => (
+      <li key={id} className="collection-item">
+        {title}
+        <i className="material-icons" onClick={() => this.onSongDelete(id)}>delete</i>
       </li>
     ));
   }
@@ -31,8 +49,21 @@ class SongList extends Component {
   }
 }
 
+// This is the mutation that will invoke the "deleteSong"
+// mutation, defined on our schema, and will delete the
+// song, given its ID.
+const mutation = gql`
+mutation DeleteSong ($id: ID) {
+  deleteSong(id: $id) {
+    id
+  }
+}
+`;
+
 // The query is executed as soon as our component is first
 // rendered. When the query is done, the component will be
 // rerendered, and the query results will be stored on the
 // "" prop
-export default graphql(fetchSongsQuery)(SongList);
+export default graphql(mutation)(
+  graphql(fetchSongsQuery)(SongList)
+);
